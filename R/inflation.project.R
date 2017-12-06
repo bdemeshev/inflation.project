244533c385f00ba18448b33d4c96d64ae475a5ad
Boris Demeshev <boris.demeshev@gmail.com>
2017-12-06 10:17:02 +0300

    first commit

---
+++ R/inflation.project.R
@@ -0,0 +1,9 @@
+#' inflation.project: project-package to forecast russian macro indicators with BVARs and alternative models
+#'
+#' Trying to test project-package idea.
+#'
+#' Trying to test project-package idea...
+#'
+#' @name inflation.project
+#' @docType package
+NULL
